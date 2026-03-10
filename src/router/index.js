c6a8fcaa8b8fde5bf0905f90b8a7fe5fb15d1999
antepapi1 <44047163+antepapi1@users.noreply.github.com>
2026-03-09 18:33:19 -0600

    Prepare deploy to GitHub Pages (hash routing + gh-pages)

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
 
@@ -8,6 +8,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
